2a44b793df628c8728bd7d1924b48e9913baa790
Shaun Curtis <shaun@coldelm.com>
2023-10-13 21:38:43 +0100

    Add project files.

---
+++ Blazr.Sitebuilder.Builder/Code/ContentRenderer.cs
@@ -0,0 +1,35 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+
+namespace Blazr.Sitebuilder.Builder.Code;
+
+public class ContentRenderer
+{
+    private readonly HtmlRenderer _htmlRenderer;
+    public ContentRenderer(HtmlRenderer htmlRenderer)
+    {
+        _htmlRenderer = htmlRenderer;
+    }
+
+    // Renders a component T which doesn't require any parameters
+    public Task<string> RenderComponent<T>() where T : IComponent
+        => RenderComponent<T>(ParameterView.Empty);
+
+    // Renders a component T using the provided dictionary of parameters
+    public Task<string> RenderComponent<T>(Dictionary<string, object?> dictionary) where T : IComponent
+        => RenderComponent<T>(ParameterView.FromDictionary(dictionary));
+
+    private Task<string> RenderComponent<T>(ParameterView parameters) where T : IComponent
+    {
+        // Use the default dispatcher to invoke actions in the context of the 
+        // static HTML renderer and return as a string
+        return _htmlRenderer.Dispatcher.InvokeAsync(async () =>
+        {
+            HtmlRootComponent output = await _htmlRenderer.RenderComponentAsync<T>(parameters);
+            return output.ToHtmlString();
+        });
+    }
+}
---
+++ Blazr.Sitebuilder.Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using Blazr.Sitebuilder.Builder.Code;
+using Blazr.Sitebuilder.Builder.Templates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<HtmlRenderer>();
+builder.Services.AddScoped<ContentRenderer>();
+
+var app = builder.Build();
+
+// Inject the renderer and optionally a name from the querystring
+app.MapGet("/", async (ContentRenderer renderer, string name = "world") =>
+{
+    // Pass the parameters and render the component
+    var html = await renderer.RenderComponent<App>(new() { { nameof(App.Name), name } });
+
+    // Return the result as HTML
+    return Results.Content(html, "text/html");
+});
+
+app.UseStaticFiles();
+
+app.Run();
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
