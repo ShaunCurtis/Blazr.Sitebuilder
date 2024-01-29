8d616b46c336a74366bc324ca8b41d524cc4d120
Shaun Curtis <shaun@coldelm.com>
2024-01-29 15:27:39 +0000

    Update

---
+++ Blazr.Sitebuilder.Builder/Drafts/Detecting-Blazor-RenderMode.cs
@@ -0,0 +1,19 @@
+﻿using Blazr.SiteBuilder;
+
+namespace Blazr.Sitebuilder.Builder;
+
+//[Route("/Articles/Detecting-Blazor-RenderMode")]
+public class Detecting_Blazor_RenderMode : ArticlesFolderBase
+{
+    public override PageData PageData => base.PageData with
+    {
+        Title = "Detecting Blazor's RenderMode",
+        PublishDate = new DateOnly(2023, 12, 14),
+        LastUpdated = new(2023, 12, 14),
+        Author = "Shaun Curtis",
+        Category = "Articles",
+        Tags = "#Blazor;",
+        Description = "Net8.0 introduced RenderNode.  Great, but how do you detect which mode you're in.",
+        MarkdownFile = "Detecting-Blazor-RenderMode.md",
+    };
+}
