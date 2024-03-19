e9d8f7f16dcbfbcadf7d34d1a30332d79d65a848
Shaun Curtis <shaun@coldelm.com>
2024-03-19 13:44:33 +0000

    Add Articles

---
+++ Blazr.Sitebuilder.Builder/Source/Posts/OnAfterRender.cs
@@ -0,0 +1,19 @@
+﻿using Blazr.SiteBuilder;
+
+namespace Blazr.Sitebuilder.Builder;
+
+[Route("/Posts/OnAfterRender")]
+public class OnAfterRender : PostsFolderBase
+{
+    public override PageData PageData => base.PageData with
+    {
+        Title = "The Trouble with OnAfterRender",
+        PublishDate = new DateOnly(2024, 03, 18),
+        LastUpdated = new(2024, 03, 18),
+        Author = "Shaun Curtis",
+        Category = "Posts",
+        Tags = "#Net; #Blazor;",
+        Description = "Why you shouldn't use OnAfterRender to mutate the state of a component.",
+        MarkdownFile = "OnAfterRender.md",
+    };
+}
