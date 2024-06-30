e4808d44e390409366d08e8b189fb8979b79cd70
Shaun Curtis <shaun@coldelm.com>
2024-06-30 22:50:30 +0100

    Update

---
+++ Blazr.Sitebuilder.Builder/Source/Stories/Blazor-Component-Tales/Blazor-Component-Tales.cs
@@ -0,0 +1,24 @@
+﻿using Blazr.Sitebuilder.Builder.Templates;
+using Blazr.SiteBuilder;
+
+namespace Blazr.Sitebuilder.Builder;
+
+[Route("/Stories/Blazor-Component-Tales/Index")]
+public class Blazor_Component_Tales_Index : StoryBase
+{
+    public override PageData PageData { get; } = new()
+    {
+        Title = "Tales from the Component Component Coalface",
+        PublishDate = new DateOnly(2024, 06, 30),
+        LastUpdated = new(2024, 06, 30),
+        Author = "Shaun Curtis",
+        Category = "Stories",
+        Story = "Tales from the Component Componenbnt Coalface",
+        Order = 1,
+        Tags = "#Blazor;#Component;",
+        Description = "Short articles on Blazor Component Development.",
+        ContentDirectory = $@".\Source\stories\Blazor-Component-Tales\",
+        MarkdownFile = "Blazor-Component-Tales.md",
+        HideInNavigationLists = true
+    };
+}
---
+++ Blazr.Sitebuilder.Builder/Source/Stories/Blazor-Component-Tales/Blazor-Component-Tales.md
@@ -0,0 +1,3 @@
+
+This is a series of short posts and notes on various topical aspects of Component design and usage.
+
