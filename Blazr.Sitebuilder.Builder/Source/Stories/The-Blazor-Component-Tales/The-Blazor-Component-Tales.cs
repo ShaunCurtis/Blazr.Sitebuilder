913b5f0c515abc3d9388d71c56ad5f6f9479ed88
Shaun Curtis <shaun@coldelm.com>
2024-07-01 21:37:03 +0100

    Update

--- Blazr.Sitebuilder.Builder/Source/Stories/The-Blazor-Component-Tales/The-Blazor-Component-Tales.cs
+++ Blazr.Sitebuilder.Builder/Source/Stories/The-Blazor-Component-Tales/The-Blazor-Component-Tales.cs
@@ -3,22 +3,22 @@ using Blazr.SiteBuilder;
 
 namespace Blazr.Sitebuilder.Builder;
 
-[Route("/Stories/Blazor-Component-Tales/Index")]
+[Route("/Stories/The-Blazor-Component-Tales/Index")]
 public class Blazor_Component_Tales_Index : StoryBase
 {
     public override PageData PageData { get; } = new()
     {
-        Title = "Tales from the Component Component Coalface",
+        Title = "The-Blazor-Component-Tales",
         PublishDate = new DateOnly(2024, 06, 30),
         LastUpdated = new(2024, 06, 30),
         Author = "Shaun Curtis",
         Category = "Stories",
-        Story = "Tales from the Component Componenbnt Coalface",
+        Story = "The-Blazor-Component-Tales",
         Order = 1,
         Tags = "#Blazor;#Component;",
         Description = "Short articles on Blazor Component Development.",
-        ContentDirectory = $@".\Source\stories\Blazor-Component-Tales\",
-        MarkdownFile = "Blazor-Component-Tales.md",
+        ContentDirectory = $@".\Source\stories\The-Blazor-Component-Tales\",
+        MarkdownFile = "The-Blazor-Component-Tales.md",
         HideInNavigationLists = true
     };
 }
